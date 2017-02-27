eb10a45058e13982d72a4bc00ec9c8d7a70b4028
Lifyrenko Anton <alifyrenko@gmail.com>
2017-02-27 12:56:54 +0200

    JpaTeamDaoImpl implemented

---
+++ trmsystem_backend/src/main/java/com/cbsgenesis/trmsystem/dao/jpa/JpaTeamDAOImpl.java
@@ -0,0 +1,65 @@
+package com.cbsgenesis.trmsystem.dao.jpa;
+
+import com.cbsgenesis.trmsystem.dao.TeamDAO;
+import com.cbsgenesis.trmsystem.model.Role;
+import com.cbsgenesis.trmsystem.model.Team;
+import org.apache.log4j.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.Collection;
+import java.util.UUID;
+
+/**
+ * Created by ANTON on 27.02.2017.
+ */
+public class JpaTeamDAOImpl implements TeamDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private final static Logger logger = Logger.getLogger(JpaTeamDAOImpl.class);
+
+
+    @Override
+    public void save(Team team) {
+        if (team.getId() == null) {
+            this.entityManager.persist(team);
+            logger.info("Team successfully saved. Team details: " + team);
+        } else {
+            this.entityManager.merge(team);
+            logger.info("Team successfully updated. Team details: " + team);
+        }
+    }
+
+    @Override
+    public Team getById(UUID id) {
+        Query query = this.entityManager.createQuery("SELECT DISTINCT team FROM Team team WHERE team.id = :id");
+        query.setParameter("id", id);
+
+        Team team = (Team) query.getSingleResult();
+        logger.info("Team successfully loaded. Team details: " + team);
+
+        return team;
+    }
+
+    @Override
+    public Collection<Team> getAll() {
+        Collection<Team> result;
+        Query query = this.entityManager.createQuery("SELECT team FROM Team team");
+        result = query.getResultList();
+
+        for (Team team : result) {
+            logger.info("Team list: " + team);
+        }
+
+        return result;
+    }
+
+    @Override
+    public void delete(Team team) {
+        this.entityManager.remove(team);
+        logger.info("Team successfully removed. Team details: " + team);
+    }
+}
