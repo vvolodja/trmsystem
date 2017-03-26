91bad244ad76e8b385a0501c2f383af6fa7d2d3c
Vasiliy Kylik <detroit.vv@gmail.com>
2017-03-27 02:24:06 +0300

    RoleValidator

--- trmsystem_backend/src/main/java/com/cbsgenesis/trmsystem/validator/RoleValidator.java
+++ trmsystem_backend/src/main/java/com/cbsgenesis/trmsystem/validator/RoleValidator.java
@@ -14,27 +14,18 @@ import java.util.HashSet;
  * Created by Vasiliy Kylik on 12.03.2017.
  */
 @Component
-public abstract class RoleValidator implements Validator {
+public class RoleValidator implements Validator {
 
-    //which objects can be validated by this validator
     @Override
     public boolean supports(Class<?> clazz) {
         return Role.class.isAssignableFrom(clazz);
     }
 
-    // Check if at least one role assign to a user
-    public void validateUserType(User user, Errors errors) {
-        if (user.getRoles().isEmpty()) {
-            user.setRoles(new HashSet<Role>());
-            errors.reject("userType", "Please Select Role");
-            // if role(s) assigned to a user, check the validity of values
-        }
-        for (Role role : user.getRoles()) {
-            if (!role.getName().equals("ROLE_USER") || !role.getName().equals("ROLE_ADMIN")) {
-                user.setRoles(new HashSet<Role>());
-                errors.reject("userType", "Please Select Correct Role");
-            }
-        }
+    @Override
+    public void validate(Object target, Errors errors) {
+        Role role = (Role) target;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userType", "required.role");
     }
 }
 
